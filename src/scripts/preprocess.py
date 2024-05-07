2ab28970edf3338bc44d6d1f76376e40db48ee95
Radhika Ravindra Halde <Radhika.Halde@course5i.com>
2024-05-07 17:46:37 +0530

    Changes made to training.py

--- src/scripts/preprocess.py
+++ src/scripts/preprocess.py
@@ -32,9 +32,10 @@ if __name__ == "__main__":
 
     # Drop several other columns
     df = df.drop(["day_charge", "eve_charge", "night_charge", "intl_charge"], axis=1)
-    print(df[["churn_True."]])
+    
     # Convert categorical variables into dummy/indicator variables.
     model_data = pd.get_dummies(df)
+    print(df[["churn_True."]])
     logger.info(model_data.columns)
 
     # Create one binary classification target column
